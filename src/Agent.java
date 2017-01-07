3cd76cf92d8d2684e94f19dea399a0c8c015c98d
soulk <fitoussi.david60@gmail.com>
2017-01-07 23:23:01 +0100

    Ajout description des actions + début de la mise en place des actions

--- src/Agent.java
+++ src/Agent.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 
 public class Agent {
 	private int posX,posY;
@@ -17,16 +18,96 @@ public class Agent {
 	}
 	
 	
-	public void update(int posX, int posY, Colour color) {
-		this.color = color;
+	public void update(int posX, int posY) {
 		this.posX = posX;
 		this.posY = posY;
 	}
 	public void decide() {
-		
+		Random r = new Random();
+		int rand = r.nextInt(7)+1;
+		switch (rand) {
+		case 1:
+			actD();
+			break;
+		case 2:
+			actDB();
+			break;
+		case 3:
+			actB();
+			break;
+		case 4:
+			actBG();
+			break;
+		case 5:
+			actG();
+			break;
+		case 6:
+			actGH();
+			break;
+		case 7:
+			actH();
+			break;
+		case 8:
+			actHD();
+			break;	
+		default:
+			break;
+		}
+	}
+	
+	private Agent getElementOn(int posX, int posY) {
+		Agent[][] tab = env.getEnv2d();
+		return tab[posX][posY];
+	}
+	private boolean isBound(int posX, int posY) {
+		if(env.getTailleX() == posX || env.getTailleY() ==posY)return true;
+		return false;
+	}
+	private void permuter(int posX, int posY) {
+		int tmpX = this.posX;
+		int tmpY = this.posY;
+		update(posX, posY);
+		env.getEnv2d()[tmpX][tmpY] = this;
+	}
+	public void actD() {
+		if(isBound(posX+1, posY)) {
+			Agent tmp = getElementOn(0, posY);
+			if(tmp == null) {
+				update(0, posY);
+			} else {
+				tmp.permuter(this.posX, this.posY);
+				update(0, posY);
+				env.getEnv2d()[0][posY] = this;
+			}
+		}
+		Agent a = getElementOn(posX+1, posY);
+		if(a == null) {
+			Agent tmp = getElementOn(0, posY);
+		}
+	}
+	public void actDB() {
 		
 	}
 	
+	public void actB() {
+		
+	}
+	public void actBG() {
+		
+	}
+	
+	public void actG() {
+		
+	}
+	public void actGH() {
+		
+	}
+	public void actH() {
+		
+	}
+	public void actHD() {
+		
+	}
 
 	public int getPosX() {
 		return posX;
--- src/Environment.java
+++ src/Environment.java
@@ -1,9 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Environment {
 	private Agent[][] tab;
-	
+	private int tailleX, tailleY;
+	private int nbAgent;
 	public Environment() {
-		
+		tab = new Agent[1][1];
+		tailleX = 10;
+		tailleY = 10;
+		nbAgent = 2;
 	}
 	
+	public void initialisation() {
+		
+	}
+	public Agent[][] getEnv2d() {
+		return tab;
+	}
+	
+	public int getTailleX() {
+		return tailleX;
+	}
+	public int getTailleY() {
+		return tailleY;
+	}
 }
